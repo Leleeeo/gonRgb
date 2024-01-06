4d6160fe719e62b4613a183f728d44bf61e9e79a
Leonardo Vieira <133938663+Leleeeo@users.noreply.github.com>
2024-01-06 14:13:06 -0300

    Add files via upload

---
+++ whiteAndBlack/scripts.js
@@ -0,0 +1,9 @@
+function black() {
+  const cor = `rgb(0, 0, 0)`;
+  document.body.style.background = cor;
+}
+
+function white() {
+  const cor = `rgb(255, 255, 255)`;
+  document.body.style.background = cor;
+}
